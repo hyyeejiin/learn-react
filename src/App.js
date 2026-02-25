3b2fddd0589242eca98b8538f300010d06389900
hyyeejiin <hyyeejiin@gmail.com>
2026-02-25 15:17:32 +0900

    오후 두번째 수업

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-// import Card from "./components/Card";
-import Counter from "./components/Counter";
+ import Card from "./components/Card";
+ import Counter from "./components/Counter";
 
 function App() {
+  const handleClick = (data) => {
+    console.log("클릭!!!", data);
+  }
+
   return (
     <div className="App">
-      <Counter />
+      <Card username={"햄찌"} onbuttonclick={handleClick}/>
+      <Card username={"혜진"} sale={50} onbuttonclick={handleClick}/>
+      <Card username={"목욜"} sale={30} onbuttonclick={handleClick}/>
+      <Card sale={0} onbuttonclick={handleClick}/>
+      {/*Counter*/}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 //import react from 'react';
 import React from 'react'
 
-const Card = () => {
+const Card = ({username = "사용자", sale = 10, onbuttonclick}) => {
     // logic
-    const username = "김햄찌";
+    //const username = "김햄찌";
     // div 태그에 card wrap 스타일 넣기
     const cardClass = "card-wrap" ;
     // view
   return (
     <div className={cardClass}>
       <p>반갑습니다. {username}님!!</p>
-      <p>할인율 10%</p>
-      <button type='button'>바로가기</button>
+      {sale && <p>할인율 {sale}%</p>}
+      <button type='button' onClick={() =>onbuttonclick(username)}>바로가기</button>
     </div>
   )
 }
